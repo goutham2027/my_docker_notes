2030a2f5737f4d5f1ae5a4471c5b27ed04154977
Goutham Pilla <goutham2027@gmail.com>
2015-11-10 01:05:36 +0530

    - Initial commit

---
+++ docker_install_ubuntu.sh
@@ -0,0 +1,7 @@
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get purge lxc-docker*
+apt-get install -y docker-engine
+service docker start
+docker run hello-world
